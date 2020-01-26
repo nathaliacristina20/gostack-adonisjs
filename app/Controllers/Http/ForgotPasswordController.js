31e9aaba49d83ecaa26e9833d8e02fc867a187cd
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-01-26 12:56:11 -0300

    Resetando senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -28,6 +29,32 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({ error: { message: 'Algo nao deu certo, esse email existe? ' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de recuperacao esta expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo nao deu certo' } })
     }
   }
 }
