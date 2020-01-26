e811570540e2dee4f01cc58ee1219b74ffa83ee8
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-01-26 12:07:58 -0300

    Recuperacao de senha

--- start/routes.js
+++ start/routes.js
@@ -3,4 +3,7 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+
 Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
