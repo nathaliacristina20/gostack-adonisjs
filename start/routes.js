24f2f381716d46eeec9e0643f54b8e9d448d4e44
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-01-26 15:23:58 -0300

    Visualizar arquivos

--- start/routes.js
+++ start/routes.js
@@ -2,11 +2,12 @@
 
 const Route = use('Route')
 
-Route.post('users', 'UserController.store')
+Route.post('/users', 'UserController.store')
 
-Route.post('sessions', 'SessionController.store')
+Route.post('/sessions', 'SessionController.store')
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('/passwords', 'ForgotPasswordController.store')
+Route.put('/passwords', 'ForgotPasswordController.update')
 
+Route.get('/files/:id', 'FileController.show')
 Route.post('/files', 'FileController.store')
