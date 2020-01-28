1798109e6b3d3ad7c97ae7ef71fd9b70d988b622
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-01-27 22:32:48 -0300

    Configurando Sentry

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Raven = require('raven')
+const Config = use('Config')
 const Env = use('Env')
 const Youch = use('youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
@@ -47,7 +49,8 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
-    console.log(error)
+    Raven.config(Config.get('services.sentry.dsn'))
+    Raven.captureException(error)
   }
 }
 
